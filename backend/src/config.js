7c5fa5fce3da2f86ee3a30f7d9ca66ae522c08bb
HopiumSwap <matski_bicknell@yahoo.com>
2023-06-30 00:14:03 -0700

    contract info
    contract info

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0x8e8F5d02D659f2Ac75066Ddfc4E1362Cff441dE1"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x8a1b2915df76eca0d97c76c389ac9dfadca101a9"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
