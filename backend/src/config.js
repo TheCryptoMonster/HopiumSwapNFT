a73329c55f0f32cbe30dde8f30633352d0e3f447
HopiumSwap <matski_bicknell@yahoo.com>
2023-07-01 21:48:15 -0700

    info
    info

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const TOKENS_PER_MINT = 100; // maximum number of NFTs a user can mint in a sing
 const PUBLIC_MINT_START_DATE = "2023-07-15T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2023-07-15T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2023-06-30T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xfB6AC10a07B5Eee144B56596a2D64bb6322FB599"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
